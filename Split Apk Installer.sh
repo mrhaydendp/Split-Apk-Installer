91c1815c733614f5aa2534ab196d58c3ef62425a
Hayden Plumley <mrhaydendp@gmail.com>
2021-11-18 09:27:16 -0800

    Error Handling
    - Added && sign after commands to stop script if not file is selected

--- Split Apk Installer.sh
+++ Split Apk Installer.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Select .apkm Files & Copy to Current Directory
-split=$(zenity --file-selection --multiple --title="Select a Split Apk File" --file-filter="*.apkm")
-echo "$split" | tr '|' '\n' | xargs -I file cp "file" ./
+split=$(zenity --file-selection --multiple --title="Select a Split Apk File" --file-filter="*.apkm") &&
+echo "$split" | tr '|' '\n' | xargs -I file cp "file" ./ &&
 
 # For Every .apkm File in Current Directory: Unzip to Split Folder, Install With "adb install-multiple", Remove Split Folder & .apkm File
 for apkm in ./*.apkm
 do
-    unzip "$apkm" -d ./Split
-    adb install-multiple ./Split/*.apk
-    rm -rf ./Split
+    unzip "$apkm" -d ./Split &&
+    adb install-multiple ./Split/*.apk &&
+    rm -rf ./Split &&
     rm "$apkm"
 done
