2ff1803c5a6f487ed460f3d3738fc85e0d3c1560
Hayden Plumley <mrhaydendp@gmail.com>
2022-11-22 16:54:02 -0800

    Update Split Apk Installer.ps1
    - Updated comments
    - Rewrote appinstaller function to be slimmer and more reliable
    - Removed all fiiles filter on file explorer

--- Split Apk Installer.ps1
+++ Split Apk Installer.ps1
@@ -1,28 +1,24 @@
-# Appinstaller Function: Detect if Selected File is an Apk or Split Apk & Adjusts Installation Method
+# Appinstaller Function: Detect if Selected File is an Apk or Split Apk & Adjust Installation Method
 function appinstaller {
-    if ($args[0] -like '*.apk'){
-        adb install -g $args[0]
-    } elseif ($args[0] -like '*.apkm'){
-        Copy-Item $args -Destination $args".zip"
-        Expand-Archive $args".zip" -DestinationPath .\Split
-        $file = (Get-ChildItem .\Split\*.apk)
-        adb install-multiple -g "$file" | Out-Host
-        Remove-Item -r .\Split
-        Remove-Item $args".zip"
-    } else {
-        Write-Host $args[0] "Is Unsupported"
+    if ("$args" -like '*.apk'){
+        adb install -r -g "$args" | Out-Host
+    } elseif ("$args" -like '*.apkm'){
+        Copy-Item "$args" -Destination "$args.zip"
+        Expand-Archive "$args.zip" -DestinationPath .\Split
+        adb install-multiple (Get-ChildItem .\Split\*apk)
+        Remove-Item -r .\Split, "$args.zip"
     }
 }
 
-# Open File Dialog
+# File Dialog
 Add-Type -AssemblyName System.Windows.Forms
 $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog
 $FileBrowser.Title = "Select Split APK(s) to Install"
-$FileBrowser.Filter = "Split APK File (*.apkm)|*.apkm|Apk File (*.apk)|*.apk|All Files (*.*)|*.*"
+$FileBrowser.Filter = "Split APK File (*.apkm)|*.apkm|Apk File (*.apk)|*.apk"
 $FileBrowser.Multiselect = "True"
 [void]$FileBrowser.ShowDialog()
 
-# Loops Appinstaller for Each File
+# Run Appinstaller Function for Each File
 foreach ($file in $FileBrowser.FileNames){
     appinstaller "$file"
 }
